84c3231fdf321a1d4e7445ff4e7e49496d3c3c58
Yang Li <liyang920407@hotmail.com>
2015-02-22 22:54:17 -0500

    add 4 modes: naive, stopping ,stemming, both

--- indexing.py
+++ indexing.py
@@ -1,101 +1,129 @@
 from collections import defaultdict
 from struct import *
+from stemming.porter2 import stem
 import load
 import os
 import re
-import unittest
 
 DEBUG = False
+modes = ('naive', 'stopping', 'stemming', 'both')
+
+
+def mem_stem(token, stem_mappings):
+    if stem_mappings.has_key(token):
+        return stem_mappings[token]
+    else:
+        stem_mappings[token] = stem(token)
+        return stem_mappings[token]
 
 # go through the whole corpus,
-# compute ttf, document length values for each distinct term
+# compute ttf for each term
+# compute doc_len for each doc
 # map doc_number to integer id
 # map term to integer id
-def prepare():
-    global ttf_counts, corpus_dir, term_ids, doc_ids, tokenize_regex
-    doc_lens = defaultdict(lambda : 0)
-    term_id = 1
-    doc_id = 1
+# stopwords should not be removed in this process
+def prepare(mode):
+    assert mode in modes
+    global ttf_counts, corpus_dir, term_ids, doc_ids, tokenize_regex, doc_lens, stem_mappings
+    term_id = doc_id = 1
     for doc_entry in load.parse_docs(corpus_dir, '', ''):
+        doc_number = doc_entry['_id']
+        print 'prepare(): ', doc_number
         doc_tokens = tokens(doc_entry['text'], tokenize_regex)
-
-        if DEBUG:
-            if doc_entry['_id'] == 'AP890622-0010':
-                print "AP890622-0010:",
-                print doc_tokens
-                exit(0)
-
         if len(doc_tokens) == 0:
             continue
-        doc_ids[doc_entry['_id']] = doc_id
+
+        doc_ids[doc_number] = doc_id
+        doc_lens[doc_id] = len(doc_tokens)
         doc_id += 1
-        doc_lens[doc_entry['_id']] = len(doc_tokens)
         for token in doc_tokens:
+            if mode in ['stemming', 'both']:
+                token = mem_stem(token, stem_mappings)
+
             ttf_counts[token] += 1
             if ttf_counts[token] == 1:
                 term_ids[token] = term_id
                 term_id += 1
+
     # write term info to file: (term, term_id)
-    with open('index/term_id_mappings.txt', "w") as f:
-        for term in term_ids.keys():
+    with open('index/{}/term_id_mappings.txt'.format(mode), "w") as f:
+        for term in term_ids:
             f.write(term + " " + `term_ids[term]` + "\n")
-    # write doc info to file: (doc_number, doc_id, token_number)
-    with open('index/doc_id_mappings.txt', "w") as f:
-        for doc_number in doc_ids.keys():
+    # write doc info to file: (doc_number, doc_id, doc_len)
+    with open('index/{}/doc_id_mappings.txt'.format(mode), "w") as f:
+        for doc_number in doc_ids:
             f.write(doc_number + " " + `doc_ids[doc_number]` + " " +
-                    `doc_lens[doc_number]` + "\n")
+                    `doc_lens[doc_ids[doc_number]]` + "\n")
 
 # return a list of tokens from text matching regex
 def tokens(text, regex):
     return [token.lower() for token in re.findall(regex, text)]
 
-def remove_front_underscore(str):
-    while str.startswith('_'):
-        str = str[1:]
-    return str
 
+def stopping_filter(token_list, stopwords):
+    return filter(lambda x: x not in stopwords, token_list)
 
 # assume our memory is large enough
-def indexing():
-    global ttf_counts, corpus_dir, term_ids, doc_ids, tokenize_regex
+def indexing(mode):
+    assert mode in modes
+    global ttf_counts, corpus_dir, term_ids, doc_ids, tokenize_regex, stopwords, stem_mappings
     index = {}
     for term in term_ids.keys():
         index[term] = {}
     for doc_entry in load.parse_docs(corpus_dir, '', ''):
         doc_number = doc_entry['_id']
-        for idx, token in enumerate(tokens(doc_entry['text'], tokenize_regex)):
-            if not index[token].has_key(doc_number):
-                index[token][doc_number] = [idx]
+        if not doc_ids.has_key(doc_number): # this doc is skipped during prepare()
+            continue
+        doc_id = doc_ids[doc_number]
+        print 'indexing():', doc_number
+        doc_tokens = tokens(doc_entry['text'], tokenize_regex)
+        for idx, token in enumerate(doc_tokens):
+            if mode in ['stopping', 'both'] and token in stopwords:
+                continue
+            if mode in ['stemming', 'both']:
+                token = mem_stem(token, stem_mappings)
+
+            if not index[token].has_key(doc_id):
+                index[token][doc_id] = [idx]
             else:
-                index[token][doc_number].append(idx)
+                index[token][doc_id].append(idx)
     # write to file
-    f = open('index/index.txt', 'wb')
+    f = open('index/{}/index.txt'.format(mode), 'wb')
     for term in index.keys():
-        print term
+        print 'writing:',term
         f.write(pack("=3L", term_ids[term], len(index[term]), ttf_counts[term]))
-        for doc_num in index[term]:
-            pos_arr = index[term][doc_num]
-            f.write(pack("=LH", doc_ids[doc_num], len(pos_arr)))
+        for doc_id in index[term]:
+            pos_arr = index[term][doc_id]
+            f.write(pack("=LH", doc_id, len(pos_arr)))
             for pos in pos_arr:
                 f.write(pack("=H", pos))
     f.close()
 
-
-
-
+def load_stopwords():
+    s = set()
+    with open('AP_DATA/stoplist.txt', 'r') as f:
+        for line in f:
+            if not line.startswith('\n'):
+                line = line.rstrip('\n')
+                s.add(line)
+    return s
 
 
 if __name__ == '__main__':
+    mode = 'stemming'
     corpus_dir = os.path.dirname(__file__) + '/AP_DATA/ap89_collection'
     ttf_counts = defaultdict(lambda : 0)
     term_ids = {}
     doc_ids = {}
-    # tokenize_regex = r"\w+(?:\.?\w+)*"
+    doc_lens = {}
+    stopwords = load_stopwords()
+    stem_mappings = {}
     tokenize_regex = r"[0-9A-Za-z]+\w*(?:\.?\w+)*"
-    prepare()
+
+    prepare(mode)
     print "term num: " + `len(term_ids)`
     print "doc num: " + `len(doc_ids)`
-    indexing()
+    indexing(mode)
 
 
 
--- query2.py
+++ query2.py
@@ -8,23 +8,27 @@ from struct import *
 import query
 import os
 import re
+import indexing
+from indexing import modes
+from stemming.porter2 import stem
 
-def recover():
+def recover(mode):
+    assert mode in modes
     global df_counts, ttf_counts, id_to_term, id_to_doc_number, doc_lens, index
     global total_docs, avg_length, voc_size
     # id_to_doc_number, doc_lens
-    with open('index/doc_id_mappings.txt', 'r') as f:
+    with open('index/{}/doc_id_mappings.txt'.format(mode), 'r') as f:
         sum_doc_len = 0
         for line in f:
             line = line.rstrip('\n')
             doc_number, doc_id, doc_len = line.split(" ")
             id_to_doc_number[int(doc_id)] = doc_number
-            doc_lens[doc_number] = int(doc_len)
+            doc_lens[int(doc_id)] = int(doc_len)
             sum_doc_len += int(doc_len)
     total_docs = len(doc_lens)
     avg_length = sum_doc_len / total_docs
     # id_to_term
-    with open('index/term_id_mappings.txt', 'r') as f:
+    with open('index/{}/term_id_mappings.txt'.format(mode), 'r') as f:
         for line in f:
             line = line.rstrip('\n')
             term, term_id = line.split(" ")
@@ -32,7 +36,7 @@ def recover():
             index[term] = {}
     voc_size = len(id_to_term)
     # df_counts, ttf_counts, index
-    with open('index/index.txt', 'rb') as f:
+    with open('index/{}/index.txt'.format(mode), 'rb') as f:
         while True:
             str = f.read(12)
             if str == "":
@@ -43,22 +47,31 @@ def recover():
             ttf_counts[term] = ttf
             for i in range(df): # how many docs
                 doc_id, tf = unpack("=LH", f.read(6))
-                doc_number = id_to_doc_number[doc_id]
                 pos_arr = unpack("="+`tf`+"H", f.read(tf * 2))
-                index[term][doc_number] = pos_arr
+                index[term][doc_id] = pos_arr
 
-def analyze_query(line, tokenize_regex):
+def analyze_query(line, tokenize_regex, mode):
+    assert mode in modes
+    global stopwords
     query_no = line.split(".")[0]
     terms_part = line[line.find(".")+1:]
     terms = re.findall(tokenize_regex, terms_part)
-    return query_no, [term.lower() for term in terms[3:]]
+    terms = [term.lower() for term in terms[3:]]
+    if mode == 'stemming':
+        terms = map(stem, terms)
+    elif mode == 'stopping':
+        terms = indexing.stopping_filter(terms, stopwords)
+    elif mode == 'both':
+        terms = indexing.stem_filter(indexing.stopping_filter(terms, stopwords))
+    return query_no, terms
 
 
-def compute_scores(query_no, query_terms):
+def compute_scores(query_no, query_terms, mode):
+    assert mode in modes
     global index, df_counts, ttf_counts, doc_lens, avg_length, total_docs, voc_size
-    okapi_tf_file = 'results2/okapi_tf.txt'
-    okapi_bm25_file = 'results2/okapi_bm25.txt'
-    unigram_lm_laplace_file = 'results2/unigram_lm_laplace.txt'
+    okapi_tf_file = 'results2/{}/okapi_tf.txt'.format(mode)
+    okapi_bm25_file = 'results2/{}/okapi_bm25.txt'.format(mode)
+    unigram_lm_laplace_file = 'results2/{}/unigram_lm_laplace.txt'.format(mode)
 
     okapi_tf_scores = defaultdict(lambda: 0.0)
     okapi_bm25_scores = defaultdict(lambda : 0.0)
@@ -68,10 +81,11 @@ def compute_scores(query_no, query_terms):
         if not index.has_key(q_term):
             continue
         print q_term,
-        for doc_number in doc_lens.keys():
-            doc_len = doc_lens[doc_number]
-            if index[q_term].has_key(doc_number):
-                pos_arr = index[q_term][doc_number]
+        for doc_id in doc_lens:
+            doc_number = id_to_doc_number[doc_id]
+            doc_len = doc_lens[doc_id]
+            if index[q_term].has_key(doc_id):
+                pos_arr = index[q_term][doc_id]
                 tf = len(pos_arr)
             else:
                 tf = 0
@@ -91,15 +105,16 @@ def compute_scores(query_no, query_terms):
         query.write_result(unigram_lm_laplace_file, query_no, unigram_lm_laplace_result[i][1], i+1,
                      unigram_lm_laplace_result[i][0])
 
-def remove_previous_results():
-    dir = os.path.dirname(__file__) + "/results2"
+def remove_previous_results(mode):
+    dir = os.path.dirname(__file__) + "/results2/{}".format(mode)
     for subdir, dirs, files in os.walk(dir):
         for file in files:
             os.remove(os.path.join(subdir, file))
 
 
 if __name__ == '__main__':
-    remove_previous_results()
+    mode = 'stemming'
+    remove_previous_results(mode)
     query_file = 'AP_DATA/query_desc.51-100.short.txt'
     tokenize_regex = r"[0-9A-Za-z]+\w*(?:\.?\w+)*"
     df_counts = {}
@@ -109,7 +124,8 @@ if __name__ == '__main__':
     doc_lens = {}
     index = {}
     avg_length = voc_size = total_docs = 0
-    recover()
+    stopwords = indexing.load_stopwords()
+    recover(mode)
     print "Recover() done!"
     print "avg_length: " + `avg_length`
     print "voc_size: " + `voc_size`
@@ -117,6 +133,6 @@ if __name__ == '__main__':
     with open(query_file) as f:
         for line in f:
             if not line.startswith("\n"):
-                query_no, terms = analyze_query(line, tokenize_regex)
+                query_no, terms = analyze_query(line, tokenize_regex, mode)
                 print "\nquery_no: " + query_no+" :",
-                compute_scores(query_no, terms)
+                compute_scores(query_no, terms, mode)
