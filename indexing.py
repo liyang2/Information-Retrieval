1914b4ba6d33f3e807ae1ac64967f1819fd238f5
Yang Li <liyang920407@hotmail.com>
2015-02-24 13:54:34 -0500

    add tokenize.py

--- indexing.py
+++ indexing.py
@@ -1,9 +1,7 @@
 from collections import defaultdict
 from struct import *
 from stemming.porter2 import stem
-import load
-import os
-import re
+import tokenize
 
 DEBUG = False
 modes = ('naive', 'stopping', 'stemming', 'both')
@@ -26,24 +24,26 @@ def prepare(mode):
     assert mode in modes
     global ttf_counts, corpus_dir, term_ids, doc_ids, tokenize_regex, doc_lens, stem_mappings
     term_id = doc_id = 1
-    for doc_entry in load.parse_docs(corpus_dir, '', ''):
-        doc_number = doc_entry['_id']
-        print 'prepare(): ', doc_number
-        doc_tokens = tokens(doc_entry['text'], tokenize_regex)
-        if len(doc_tokens) == 0:
-            continue
+    with open(tokenize.token_file, 'r') as f:
+        for line in f:
+            items = line.rstrip('\n').split(' ')
+            doc_number = items[0]
+            print 'prepare(): ', doc_number
+            doc_tokens = items[1:]
+            if len(doc_tokens) == 0:
+                continue
 
-        doc_ids[doc_number] = doc_id
-        doc_lens[doc_id] = len(doc_tokens)
-        doc_id += 1
-        for token in doc_tokens:
-            if mode in ['stemming', 'both']:
-                token = mem_stem(token, stem_mappings)
+            doc_ids[doc_number] = doc_id
+            doc_lens[doc_id] = len(doc_tokens)
+            doc_id += 1
+            for token in doc_tokens:
+                if mode in ['stemming', 'both']:
+                    token = mem_stem(token, stem_mappings)
 
-            ttf_counts[token] += 1
-            if ttf_counts[token] == 1:
-                term_ids[token] = term_id
-                term_id += 1
+                ttf_counts[token] += 1
+                if ttf_counts[token] == 1:
+                    term_ids[token] = term_id
+                    term_id += 1
 
     # write term info to file: (term, term_id)
     with open('index/{}/term_id_mappings.txt'.format(mode), "w") as f:
@@ -55,12 +55,10 @@ def prepare(mode):
             f.write(doc_number + " " + `doc_ids[doc_number]` + " " +
                     `doc_lens[doc_ids[doc_number]]` + "\n")
 
-# return a list of tokens from text matching regex
-def tokens(text, regex):
-    return [token.lower() for token in re.findall(regex, text)]
 
 def stopping_filter(token_list, stopwords):
     return filter(lambda x: x not in stopwords, token_list)
+
 
 # k means how many terms to index during each iteration of corpus
 def indexing(mode, k):
@@ -69,7 +67,7 @@ def indexing(mode, k):
 
     progress = 0
     cf = open('index/{}/category.txt'.format(mode), 'w')
-    f = open('index/{}/index.txt'.format(mode), 'wb')
+    idxf = open('index/{}/index.txt'.format(mode), 'wb')
     while progress < len(ttf_counts):
         print 'progress:',
         index = defaultdict(lambda : {})
@@ -78,13 +76,15 @@ def indexing(mode, k):
         print progress
 
         # go through the whole corpus
-        for doc_entry in load.parse_docs(corpus_dir, '', ''):
-            doc_number = doc_entry['_id']
+        tkf = open(tokenize.token_file, 'r')
+        for line in tkf:
+            items = line.rstrip('\n').split(' ')
+            doc_number = items[0]
             if not doc_ids.has_key(doc_number): # this doc is skipped during prepare()
                 continue
             doc_id = doc_ids[doc_number]
             # print 'indexing():', doc_number
-            doc_tokens = tokens(doc_entry['text'], tokenize_regex)
+            doc_tokens = items[1:]
             for idx, token in enumerate(doc_tokens):
                 if mode in ['stopping', 'both'] and token in stopwords:
                     continue
@@ -97,23 +97,27 @@ def indexing(mode, k):
                     index[token][doc_id] = [idx]
                 else:
                     index[token][doc_id].append(idx)
+        tkf.close()
 
         # write to file
         for term in index:
             # print 'writing:',term
-            begin_offset = f.tell()
-            f.write(pack("=3L", term_ids[term], len(index[term]), ttf_counts[term]))
+            begin_offset = idxf.tell()
+            idxf.write(pack("=3L", term_ids[term], len(index[term]), ttf_counts[term]))
             for doc_id in index[term]:
                 pos_arr = index[term][doc_id]
-                f.write(pack("=LH", doc_id, len(pos_arr)))
+                idxf.write(pack("=LH", doc_id, len(pos_arr)))
                 for pos in pos_arr:
-                    f.write(pack("=H", pos))
-            end_offset = f.tell()
-            cf.write(" ".join([term, `begin_offset`, `end_offset`]))
+                    idxf.write(pack("=H", pos))
+            end_offset = idxf.tell()
+            cf.write(" ".join([term, `begin_offset`, `end_offset`]) + '\n')
 
         progress += k
-    f.close()
+        if progress == k * 10:
+            break
+    idxf.close()
     cf.close()
+
 
 def load_stopwords():
     s = set()
@@ -125,19 +129,22 @@ def load_stopwords():
     return s
 
 
-if __name__ == '__main__':
-    mode = 'naive'
-    corpus_dir = os.path.dirname(__file__) + '/AP_DATA/ap89_collection'
-    ttf_counts = defaultdict(lambda : 0)
-    term_ids = {}
-    doc_ids = {}
-    doc_lens = {}
-    stopwords = load_stopwords()
-    stem_mappings = {}
-    tokenize_regex = r"[0-9A-Za-z]+\w*(?:\.?\w+)*"
+mode = 'naive'
+ttf_counts = defaultdict(lambda : 0)
+term_ids = {}
+doc_ids = {}
+doc_lens = {}
+stopwords = load_stopwords()
+stem_mappings = {}
 
+def main():
     prepare(mode)
     print "term num: " + `len(term_ids)`
     print "doc num: " + `len(doc_ids)`
     indexing(mode, 1000)
+
+if __name__ == '__main__':
+    main()
+
+
 
---
+++ tokenize.py
@@ -0,0 +1,20 @@
+import load
+import re
+
+# process the whole collection of docs
+# keep only DOCNO and TEXT fields and tokenize TEXT string
+
+tokenize_regex = r"[0-9A-Za-z]+\w*(?:\.?\w+)*"
+token_file = 'AP_DATA/ap89_collection/ap00all.txt'
+corpus_dir = 'AP_DATA/ap89_collection'
+
+# return a list of tokens from text matching regex
+def tokens(text, regex):
+    return [token.lower() for token in re.findall(regex, text)]
+
+if __name__ == '__main__':
+    with open(token_file, 'w') as f:
+        for doc_entry in load.parse_docs(corpus_dir, '', ''):
+            doc_number = doc_entry['_id']
+            doc_tokens = tokens(doc_entry['text'], tokenize_regex)
+            f.write(doc_number + " " + ' '.join(doc_tokens) + '\n')
